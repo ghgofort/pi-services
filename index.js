84032d04a2a911bae91ca276c98ab36a064d2153
ghgofort <galengoforth@gmail.com>
2023-09-27 13:29:09 -0400

    updates to logic to export express app.

--- index.js
+++ index.js
@@ -8,16 +8,10 @@ const routes = require('./routes');
 
 // Import the express module.
 const express = require('express'); 
+// Create an instance of express.
+const app = express();
 
-function main() {
-  // Create an instance of express.
-  const app = express();
+// Setup the routes for the application.
+app.use('/', routes);
 
-  // Setup the routes for the application.
-  app.use('/', routes);
-
-  // Start the server.
-  app.listen(3000, () => console.log('Example app listening on port 3000!'));
-}
-
-module.exports = main;
+module.exports = app;
