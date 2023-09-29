438bec2a91c63e6c47273bce7294fc4c9ca56e51
ghgofort <galengoforth@gmail.com>
2023-09-29 13:57:36 -0400

    fix bad suggestion merged

--- routes.js
+++ routes.js
@@ -9,37 +9,17 @@ router.get('/', (req, res) => {
 
 /** API endpoint for getting all items in the HPT_Readings collection. */
 router.get('/hpt', (req, res, next) => {
-   const firebaseService = require('./services/firebaseService');
-   firebaseService.hpt().then((data) => {;const express = require('express');
-const router = express.Router();
-
-// Define your routes here
-router.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-
-/** API endpoint for getting all items in the HPT_Readings collection. */
-router.get('/hpt', (req, res, next) => {
-   const firebaseService = require('./services/firebaseService');
-   firebaseService.hpt().then((data) => {;
-      res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000, https://galengoforth.com, *');
-      res.setHeader('Access-Control-Allow-Methods', 'GET');
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-      res.send(data);
+  const firebaseService = require('./services/firebaseService');
+  firebaseService.hpt().then((data) => {
+    ;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.send(data);
   }).catch((e) => {
-      console.log(e);
+    console.log(e);
   }).finally(() => {
-      next();
-  });
-});
-
-module.exports = router;
-      res.send(data);
-  }).catch((e) => {
-      console.log(e);
-  }).finally(() => {
-      next();
+    next();
   });
 });
 
