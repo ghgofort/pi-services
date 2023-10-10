2ca2f8599327f8fd004ac2d7e4777ccfd732d4ab
ghgofort <galengoforth@gmail.com>
2023-10-10 13:53:35 -0400

    update to get all configs from environment vars, and add param to server method to allow override of number or results returned.

--- services/firebaseService.js
+++ services/firebaseService.js
@@ -8,13 +8,19 @@
  * This function is responsible for getting the data from the Firebase database
  * and sending it to the client.
  */
-async function hpt() {
+async function hpt(count = process.env.FB_DEFAULT_RECORDS) {
     // Initialize the firestore DB.
     const { collection, getDocs, getFirestore, limit, orderBy, query } = require('firebase/firestore');
-    const hptConfig = require('./hpt-config.json');
     const { initializeApp } = require('firebase/app');
-    hptConfig.apiKey = process.env.FB_API_KEY;
-    hptConfig.appId = process.env.FB_APP_ID;
+    const hptConfig = {
+        apiKey: process.env.FB_API_KEY,
+        authDomain: process.env.FB_AUTH_DOMAIN,
+        projectId: process.env.FB_PROJECT_ID,
+        storageBucket: process.env.FB_STORAGE_BUCKET,
+        messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+        databaseURL: process.env.FB_DATABASE_URL
+    };
+
     const app = initializeApp(hptConfig);
     const db = getFirestore(app);
     let response = [];
@@ -22,7 +28,7 @@ async function hpt() {
     // Get data from Firestore database & send it to the client.
     try {
         const hptRef = collection(db, 'HPT_Readings');
-        const q = query(hptRef, orderBy('dateTimeCreated', 'desc'), limit(12));
+        const q = query(hptRef, orderBy('dateTimeCreated', 'desc'), limit(count));
         const hptSnapshot = await getDocs(q);
         hptSnapshot.forEach(doc => {
             response.push(doc.data());
